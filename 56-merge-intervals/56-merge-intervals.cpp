9af5690294028857075cb1c35e4c1d5fd96223cb
lakshay-16 <75731309+lakshay-16@users.noreply.github.com>
2022-05-28 03:58:06 +0530

    Time: 39 ms (86.61%), Space: 19.1 MB (67.57%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
+    static bool comp(vector<int> &a,vector<int> &b)
+{
+    if(a[0]==b[0])
+    return a[1]<b[1];
+    
+    return a[0]<b[0];
+}
+    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-         int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
-        
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        vector<vector<int>> ans;
-        
-       for(int i=1;i<n;i++)
-       {
-           if(end>=intervals[i][0])
-           end=max(end,intervals[i][1]);
-           
-           else
-           {
-               ans.push_back({start,end});
-               start=intervals[i][0];
-               end=intervals[i][1];
-           }
-           
-       }
-        
-        ans.push_back({start, end});
-        return ans;
+             int n=intervals.size();
+         vector<vector<int>> ans;
+         int x,j;
+         
+         sort(intervals.begin(),intervals.end(),comp);
+         
+         x=intervals[0][0];
+         j=intervals[0][1];
+         
+         for(int i=1;i<intervals.size();i++)
+         {
+             
+             if(j>intervals[i][0]&&j>=intervals[i][1])
+             continue;
+             
+            else if(j>=intervals[i][0]&&j<=intervals[i][1])
+             {
+             j=intervals[i][1];
+             continue;
+             }
+         
+         else
+         {
+             ans.push_back({x,j});
+             x=intervals[i][0];
+             j=intervals[i][1];
+         }
+             
+         }
+         
+         
+         ans.push_back({x,j});
+         
+         return ans;
     }
 };
