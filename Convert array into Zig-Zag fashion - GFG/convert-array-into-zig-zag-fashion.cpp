e9458998093bb24924e210b2da67706fb6714c22
lakshay-16 <75731309+lakshay-16@users.noreply.github.com>
2022-06-04 12:07:45 +0530

    Added solution - LeetHub

---
+++ Convert array into Zig-Zag fashion - GFG/convert-array-into-zig-zag-fashion.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Program for zig-zag conversion of array
+	void swap(int &a,int &b)
+	{
+	    int temp=a;
+	    a=b;
+	    b=temp;
+	    
+	}
+	
+	void zigZag(int arr[], int n) {
+	    // code here
+	    bool flag=1;
+	    
+	    for(int i=0;i<n-1;i++)
+	    {
+	    
+	    if(flag&&arr[i]>arr[i+1])
+	    swap(arr[i],arr[i+1]);
+	    
+	    else if(!flag&&arr[i]<arr[i+1])
+	    swap(arr[i],arr[i+1]);
+	    
+	    flag=flag^1;
+	    }
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.zigZag(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
