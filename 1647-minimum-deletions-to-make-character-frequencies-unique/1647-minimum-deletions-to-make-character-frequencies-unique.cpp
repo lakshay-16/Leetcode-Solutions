2310758ef6bd421264c09b0cf063436cac83ae3a
lakshay-16 <75731309+lakshay-16@users.noreply.github.com>
2022-06-29 03:36:58 +0530

    Time: 67 ms (74.64%), Space: 17.4 MB (37.41%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        
+      
         vector<int> freq(26,0);
         for(int i=0;i<s.length();i++)
             freq[s[i]-'a']++;
@@ -11,6 +11,7 @@ public:
         
         for(int i=0;i<26;i++)
         {
+            
             while(freq[i]&&f.find(freq[i])!=f.end())
             {
                 freq[i]--;
@@ -19,8 +20,7 @@ public:
             
             f.insert(freq[i]);
         }
-        
-        
+      
         return ans;
     }
 };
