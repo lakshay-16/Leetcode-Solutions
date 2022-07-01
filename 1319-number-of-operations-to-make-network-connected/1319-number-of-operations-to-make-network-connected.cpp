f3955e17ee86a846471fe38e3141916012f459c7
lakshay-16 <75731309+lakshay-16@users.noreply.github.com>
2022-07-02 01:52:50 +0530

    Time: 234 ms (38.13%), Space: 50.8 MB (55.82%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,57 +1,44 @@
 class Solution {
 public:
-    void dfs(vector<int> adj[],int node,vector<bool> &vis)
+    void dfs(int node,vector<int> &vis,vector<int> adj[])
     {
         vis[node]=true;
         
         for(auto x:adj[node])
         {
             if(!vis[x])
-                dfs(adj,x,vis);
+                dfs(x,vis,adj);
         }
-        
     }
     
-    int makeConnected(int n, vector<vector<int>>& c) {
+    int makeConnected(int n, vector<vector<int>>& connections) {
         
+        int edges=connections.size();
+        
+        if(edges<n-1)
+            return -1;
+        
+        vector<int> vis(n,0);
         vector<int> adj[n];
-        int e=c.size();
-        for(int i=0;i<e;i++)
+        
+        for(int i=0;i<connections.size();i++)
         {
-            adj[c[i][0]].push_back(c[i][1]);
-            adj[c[i][1]].push_back(c[i][0]);
+            adj[connections[i][0]].push_back(connections[i][1]);
+             adj[connections[i][1]].push_back(connections[i][0]);
         }
         
-        vector<bool> vis(n,false);
         int count=0;
         
         for(int i=0;i<n;i++)
         {
             if(!vis[i])
-            {
-                dfs(adj,i,vis);
-                count++;
+            {dfs(i,vis,adj);
+            count++;
             }
-            
         }
         
+        int dif=count-1;
         
-        if(count==1)
-            return 0;
-        
-        
-        int extra=count-1;
-        int grouped=n-extra;
-        
-        if(e-grouped+1>=extra)
-            return extra;
-        
-        
-        
-        
-        
-        return -1;
-        
-        
+        return dif;
     }
 };
