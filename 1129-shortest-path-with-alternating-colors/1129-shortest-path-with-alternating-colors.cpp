193eac0ce74e8c02230d05c5392c7f67074aa558
lakshay-16 <75731309+lakshay-16@users.noreply.github.com>
2022-07-04 02:12:24 +0530

    Time: 25 ms (75.22%), Space: 14.7 MB (76.83%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public: 
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+         vector<vector<pair<int,int>>> graph(n);
+        for(auto &e:redEdges)
+            graph[e[0]].push_back({e[1],1});
+         for(auto &e:blueEdges)
+            graph[e[0]].push_back({e[1],-1});
+        
+        vector<int> dRed(n,INT_MAX-1),dBlue(n,INT_MAX-1);dRed[0]=0;dBlue[0]=0;
+        
+        queue<int> q;
+        q.push(0);
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            
+            for(auto &[child,color]:graph[node])
+            {
+                if(color==1&&dBlue[node]+1<dRed[child])
+                {
+                    dRed[child]=dBlue[node]+1;
+                    q.push(child);
+                }
+                
+                 else if(color==-1&&dRed[node]+1<dBlue[child])
+                {
+                    dBlue[child]=dRed[node]+1;
+                    q.push(child);
+                }
+                
+                
+            }
+        }
+     
+        
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {
+            int temp=min(dRed[i],dBlue[i]);
+            ans.push_back((temp==INT_MAX-1)?-1:temp);
+        }
+        
+        return ans;
+    }
+};
