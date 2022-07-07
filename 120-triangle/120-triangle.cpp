8a5579b651f6ec042c452c2c9ab3e28feb21e2ec
lakshay-16 <75731309+lakshay-16@users.noreply.github.com>
2022-07-08 02:51:44 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (7.86%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int solve(int index,int level,vector<vector<int>> &dp,vector<vector<int>> &g)
+    {
+        if(index<0||index>=g[level].size())
+            return 1e9;
+       
+        if(dp[level][index]!=-1)
+            return dp[level][index];
+        
+        if(level==0)
+            return g[level][index];
+        
+        int prev=g[level][index]+solve(index-1,level-1,dp,g);
+        int cur=g[level][index]+solve(index,level-1,dp,g);
+        
+        
+        return dp[level][index]=min(prev,cur);
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+       int n=triangle.size();
+        int ans=INT_MAX;
+        
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        
+        for(int i=0;i<n;i++)
+            ans=min(ans,solve(i,n-1,dp,triangle));
+        
+        return ans;
+    }
+};
