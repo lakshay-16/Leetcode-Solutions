850dd3d9c455c814fd4ecc1bb5ba90ba6f29ee0d
lakshay-16 <75731309+lakshay-16@users.noreply.github.com>
2022-07-24 15:20:29 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.06%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,19 +4,19 @@ public:
         
         vector<vector<int>> ans;
         
-        for(int i=0;i<numRows;i++)
+        for(int i=1;i<=numRows;i++)
         {
-            vector<int> g(i+1,1);
+            vector<int> g(i,1);
             ans.push_back(g);
         }
         
         
-        for(int i=0;i<numRows;i++)
+        for(int i=2;i<ans.size();i++)
         {
             for(int j=1;j<ans[i].size()-1;j++)
                 ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
         }
         
-        return ans;
+     return ans;
     }
 };
