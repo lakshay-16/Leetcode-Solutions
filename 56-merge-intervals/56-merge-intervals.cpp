2198be4a71a40072887199524e834ffa989e45ca
lakshay-16 <75731309+lakshay-16@users.noreply.github.com>
2022-07-26 13:59:31 +0530

    Time: 61 ms (48.13%), Space: 19 MB (66.50%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
     static bool comp(vector<int> &a,vector<int> &b)
-{
-    if(a[0]==b[0])
-    return a[1]<b[1];
-    
-    return a[0]<b[0];
-}
+    {
+        if(a[0]==b[0])
+            return a[1]<b[1];
+        
+        return a[0]<b[0];
+    }
     
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-             int n=intervals.size();
-         vector<vector<int>> ans;
-         int x,j;
-         
-         sort(intervals.begin(),intervals.end(),comp);
-         
-         x=intervals[0][0];
-         j=intervals[0][1];
-         
-         for(int i=1;i<intervals.size();i++)
-         {
-             
-             if(j>intervals[i][0]&&j>=intervals[i][1])
-             continue;
-             
-            else if(j>=intervals[i][0]&&j<=intervals[i][1])
-             {
-             j=intervals[i][1];
-             continue;
-             }
-         
-         else
-         {
-             ans.push_back({x,j});
-             x=intervals[i][0];
-             j=intervals[i][1];
-         }
-             
-         }
-         
-         
-         ans.push_back({x,j});
-         
-         return ans;
+        vector<vector<int>> ans;
+        
+        sort(intervals.begin(),intervals.end(),comp);
+        
+         int start=intervals[0][0];
+        int end=intervals[0][1];
+        
+        
+        for(int i=1;i<intervals.size();i++)
+        {
+            if(end>=intervals[i][1])
+                continue;
+            
+            else if(end>=intervals[i][0])
+            {
+                end=intervals[i][1];
+                continue;
+            }
+          
+            else
+            {
+                ans.push_back({start,end});
+                start=intervals[i][0];
+                end=intervals[i][1];
+            }
+        }
+        
+        ans.push_back({start,end});
+        
+        return ans;
     }
 };
