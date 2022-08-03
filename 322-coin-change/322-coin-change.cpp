d36e3b4d5552ba95b839a41e6defe7ab29d4e222
lakshay-16 <75731309+lakshay-16@users.noreply.github.com>
2022-08-03 22:53:13 +0530

    Time: 190 ms (29.30%), Space: 45.6 MB (23.66%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int solve(int amount,int index,vector<int> &count,vector<vector<int>> &dp)
+    int solve(int index,int val,vector<int> &g,vector<vector<int>> &dp)
     {
-        if(index==0)
-        {
-            if(amount%count[0]==0) return amount/count[0];
-                
-            return 1e9;
-        }
+       if(index==0)
+       {
+           if(val%g[0]==0)return val/g[0];
+           
+           return 1e9;
+       }
         
-        if(dp[index][amount]!=-1)
-            return dp[index][amount];
+        if(dp[index][val]!=-1)
+            return dp[index][val];
         
-        int nottake=0+solve(amount,index-1,count,dp);
-        int take=INT_MAX;
-        if(amount>=count[index])
-            take=1+solve(amount-count[index],index,count,dp);
+        int nottake=0+solve(index-1,val,g,dp);
+        int take=1e9;
+        if(val>=g[index])
+            take=1+solve(index,val-g[index],g,dp);
         
-        return dp[index][amount]=min(take,nottake);
+        return dp[index][val]=min(take,nottake);
     }
     
     int coinChange(vector<int>& coins, int amount) {
+        
         int n=coins.size();
         vector<vector<int>> dp(n,vector<int> (amount+1,-1));
         
-        int ans=solve(amount,n-1,coins,dp);
+        int ans=solve(n-1,amount,coins,dp);
         
         return (ans==1e9)?-1:ans;
     }
